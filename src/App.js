4aa48544e6e9d8e1a3dd62cb55741f1ea4e53cba
0xBalance <1137774080@qq.com>
2023-02-28 19:59:17 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import routes from './pages/index'
+import { Web3ReactProvider, useWeb3React } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useEagerConnect, useInactiveListener } from './web3/useMetaConnect'
+import { WalletProvider } from '@suiet/wallet-kit'
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import '@suiet/wallet-kit/style.css'
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 5000
+  return library
+}
+function App() {
+  return (
+    <WalletProvider getLibrary={getLibrary}>
+      <Routes>
+        {routes.map((data, index) => (
+          <Route
+            onUpdate={() => window.scrollTo(0, 0)}
+            exact={true}
+            path={data.path}
+            element={data.component}
+            key={index}
+          />
+        ))}
+      </Routes>
+
+      <ToastContainer autoClose={2000} />
+    </WalletProvider>
+  )
+}
+
+export default App
---
+++ src/components/header/Header/Header.jsx
@@ -0,0 +1,149 @@
+import React, { useRef, useState, useEffect } from 'react'
+import DarkMode from '../DarkMode'
+import styles from './header.module.scss'
+// import './header.module.less'
+import LogoIcon from '../../../assets/img/logo/logo.png'
+import searchIcon from '../../../assets/img/header/icon-search.png'
+import classNames from 'classnames'
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+import { Box } from '@mui/material'
+import { ConnectButton } from '@suiet/wallet-kit';
+const Header = () => {
+  const headerRef = useRef(null)
+  const navigate = useNavigate()
+  const tabList = [
+    {
+      key: 'Launchpad',
+      name: 'Launchpad',
+      link: '/launchpad'
+    },
+    {
+      key: 'Marketplace',
+      name: 'Marketplace',
+      link: '/marketplace'
+    },
+    {
+      key: 'Stats',
+      name: 'Stats',
+      link: '/stats'
+    },
+    {
+      key: 'Airdrop',
+      name: 'Airdrop',
+      link: '/airdrop'
+    }
+  ]
+  useEffect(() => {
+    window.addEventListener('scroll', isSticky)
+    return () => {
+      window.removeEventListener('scroll', isSticky)
+    }
+  })
+  const isSticky = () => {
+    const header = document.querySelector('.js-header')
+    const scrollTop = window.scrollY
+    scrollTop >= 300 ? header.classList.add('is-fixed') : header.classList.remove('is-fixed')
+    scrollTop >= 400 ? header.classList.add('is-small') : header.classList.remove('is-small')
+  }
+
+  const menuLeft = useRef(null)
+  const btnToggle = useRef(null)
+  const btnSearch = useRef(null)
+
+  const [activeIndex, setActiveIndex] = useState(0)
+  const handleOnClick = index => {
+    if (index) {
+      toast.info(
+        <Box sx={{
+          fontFamily: 'Montserrat Bold',
+          fontWeight: 700,
+          fontSize: 20
+        }}>
+          Coming soon
+        </Box>
+      )
+      return
+    }
+    navigate('/')
+    setActiveIndex(index)
+  }
+
+  return (
+    <header id='header_main' className={classNames(styles.header, 'js-header')} ref={headerRef}>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <Link to="/"><img className={styles.logo} src={LogoIcon} alt='' /></Link>
+          <div className={styles.searchInput}>
+            <img src={searchIcon} alt='' className={styles.searchIcon} />
+            <input className={styles.input} type='text' placeholder='Type to search...' />
+          </div>
+          <div className={styles.tabList}>
+            {tabList.map((item, index) => (
+              <div
+                key={item.key}
+                className={classNames(styles.item, activeIndex === index && styles.active)}
+                onClick={() => {
+                  handleOnClick(index)
+                }}
+              >
+                {item.name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <Box
+          className='connect-button'
+          sx={{
+            '.wkit-connected-container': {
+              width: 200,
+              background: 'linear-gradient(90deg, #1785FF 0%, #9414FF 100%)',
+              backdropFilter: 'blur(20px)',
+              borderRadius: '33.6px',
+              color: '#fff',
+              // height: 40,
+              '.wkit-connected-button': {
+                '.wkit-connected-button__balance': {
+                  fontFamily: 'Montserrat',
+                  fontSize: '12px',
+                  color: '#fff',
+                },
+                '.wkit-address-select': {
+                  span: {
+                    fontFamily: 'Montserrat',
+                    fontSize: '12px',
+                    color: '#fff',
+                  }
+                },
+                '&:hover': {
+                  border: 'none'
+                },
+                '&:focus': {
+                  border: 'none'
+                }
+              },
+            },
+            '.wkit-button': {
+              width: 200,
+              background: 'linear-gradient(90deg, #1785FF 0%, #9414FF 100%)',
+              backdropFilter: 'blur(20px)',
+              borderRadius: '33.6px',
+              fontFamily: 'Montserrat',
+              color: '#fff',
+              fontSize: '16px',
+              '&:hover': {
+                border: 'none',
+                color: '#fff'
+              }
+            }
+          }}
+        >
+          <ConnectButton label="Connect Wallet" />
+        </Box>
+        {/* <DarkMode /> */}
+      </div>
+    </header >
+  )
+}
+
+export default Header

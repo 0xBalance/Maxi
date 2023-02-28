932476fe29cbb461f8be57b7d8316cc2450fd965
0xBalance <1137774080@qq.com>
2023-02-28 22:54:56 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+/*
+ * @Descripttion : EsSwap
+ * @version      : 1.0.0
+ * @Author       : 0xBalance
+ * @Date         : 2023-02-27 00:07:49
+ * @LastEditors  : 0xBalance
+ * @LastEditTime : 2023-02-28 22:50:33
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
 
 ReactDOM.render(
